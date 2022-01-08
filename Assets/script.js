a537a7c9959d89f62b83fc301bc1dc23f973c228
Fernando Vasquez <vasquezjrf@gmail.com>
2022-01-07 20:37:00 -0500

    cleaned up unused code, added comments for script

--- Assets/script.js
+++ Assets/script.js
@@ -6,8 +6,6 @@ var hourBlock = $('<div>');
 //text inputs access
 var textInputDiv = $('<div>');
 var textInput = $('<input type="text" placeholder="Enter Planned Activity">');
-//save buttons access
-// var saveBtn = $('<button>');
 
 //hour objects array
 var hours = [
@@ -68,33 +66,44 @@ var hours = [
   }
 ]
 
+//logic to make timeblock rows 
 hours.forEach(function(hour){
-  var momentHour = moment().hours()
+  //set a variable equal to the current time to be used as a parameter for determining color of timeblock 
+  //for testing purposes, change value of var MomentHour to a number between 9 and 17 (9am - 5 pm)
+  var momentHour = 9
+  //create text inputs
   var textInputDiv = $('<textarea>'); 
+  //color code rows depending on if hour block matches current hour, is in the past, or is in the future
   if (momentHour > hour.timeValue) {
+    //assign classes depending on if hour in hour block of each timeblock is in the past, present, or future
     textInputDiv.addClass('col-8 description past')
   } else if (momentHour === hour.timeValue) {
     textInputDiv.addClass('col-8 description present')
   } else {
     textInputDiv.addClass('col-8 description future')
   }
-
+//create rows
   divRow = $('<div>');
   divRow.addClass('row');
+  //assign each row an ID associated to the time value (9 am, 10 am, 11 am, etc) of each associated hourblock
   divRow.attr('id', hour.timeValue);
   containerEl.append(divRow);
   hourBlock = $('<div>');
   hourBlock.addClass('col-1 hour');
+  //make each hour block show the correct time value
   hourBlock.text(hour.displayHour);
- 
+ //retrieve entered activity from local storage
   textInputDiv.val(localStorage.getItem(hour.timeValue))
   textInputDiv.addClass('col-8 description');
+  //generate save buttons
   var saveBtn = $('<button>');
-  saveBtn.text('Save');
+  saveBtn.text('Click here to save');
+  //place all created elements into a row, rows are then placed into the container element
   containerEl.append(divRow.append(hourBlock, textInputDiv, saveBtn));
-  saveBtn.addClass('save');
+  saveBtn.addClass('saveBtn');
 })
 
+//save activity to local storage
 $('.save').on('click', function(){
   var activity = $(this).siblings('.description').val()
 var hourKey = $(this).parent().attr('id');
@@ -102,70 +111,6 @@ localStorage.setItem(hourKey, activity);
 
 //this = save button
 })
-
-
-
-
-//want dive to have three children
-//time, input, save button
-
-
-
-
-
-// var targetColor;
-// console.log(changeColors());
-//for loop to make rows dynamically
-// for (var i = 0; i < hours.length; i++) {
-//   divRow = $('<div>');
-//   divRow.addClass('row');
-//   divRow.attr('value', hours[i].timeValue);
-//   containerEl.append(divRow);
-//   //add hourblocks
-//   hourBlock = $('<div>');
-//   hourBlock.addClass('col hour');
-//   //assign text to hour blocks and determine if am or pm
-//   hourBlock.text(hours.displayHour);
-//   // if (hours[i].displayHour[i] === 9 || hours[i].displayHour[i] === 10 || hours[i].displayHour[i] === 11) {
-//   //   hourBlock = hourBlock.text(hours.displayHour[i] + minutes + ' ' + hours.meridiem[0])
-//   //   console.log(hourBlock)
-//   // } else if (hours[i].displayHour[i] === 12 || hours[i].displayHour[i] === 1 || hours[i].displayHour[i] === 2 || hours[i].displayHour[i] === 3 || hours[i].displayHour[i] === 4 || hours[i].displayHour[i] === 5) {
-//   //   hourBlock = hourBlock.text(hours.hour[i] + minutes + ' ' + hours.meridiem[1])
-//   //   console.log(hourBlock)
-//   // };
-//   //hourblock styles
-//   hourBlock.css('border', 'black solid 5px');
-//   hourBlock.css('font-size', '40px');
-//   hourBlock.css('text-align', 'right');
-//   hourBlock.css('height', '60px');
-//   divRow.append(hourBlock);
-
-//   //add text inputs
-//   textInputDiv = $('<div>');
-//   textInputDiv.addClass(`input-group input-group-lg col-6`);
-//   textInputDiv.attr('style', `background-color: ${targetColor}`)
-//   divRow.append(textInputDiv)
-//   textInput = $('<input type="text" placeholder="Enter Planned Activity">');
-//   textInput.addClass(`form-control col input-group-text`);
-//   textInput.css('height', '60px');
-//   textInputDiv.append(textInput);
-
-//   // add save buttons
-//   saveBtn = $('<button>');
-//   saveBtn.addClass('col saveBtn');
-//   saveBtn.text('SAVE BUTTON PLACEHOLDER!');
-//   saveBtn.css('font-size', '25px');
-//   saveBtn.css('text-align', 'right');
-//   saveBtn.css('height', '60px');
-//   saveBtn.css('text-align', 'center');
-//   divRow.append(saveBtn);
-
-// }
-//END OF FUNCTION
-
-
-
-
 
 
 
@@ -189,42 +134,12 @@ function changeColors() {
   )
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//make function to generate rows
-var makeTimeBlocks = function () {
-
-}
-
-
-
-//for each function on hours array
-
-
-//include function to change class depending on past/current/future
-
-//make function to color code objects
-
-//function to load local storage
-
-//function to save to local storage
-
 //function to show current date and time in jumbotron via moment()
 function currentDateandTime() {
   var currentDay = moment().format('dddd, MMMM Do')
   $("#currentDay").text(currentDay);
 }
+
 //calls date/time function
 currentDateandTime();
+
